5b106f4d17ab6fdfcfa8aaebafe3ea8dc1fb08f0
Fabian Damken <fabian.damken@frisp.org>
2020-11-03 13:24:57 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Matplotlib TUDa
+
+This is a small packages that adds the corporate colors of the TU Darmstadt (see [Corporate Design](https://www.tu-darmstadt.de/media/medien_stabsstelle_km/services/medien_cd/das_bild_der_tu_darmstadt.pdf), pages 18, 19) to Matplotlib.
---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='matplotlib-tuda',
+    version='0.0.1',
+    author='Fabian Damken',
+    author_email='fabian.damken@frisp.org',
+    description='Small package to add the corporate colors of the TU Darmstadt to Matplotlib.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fdamken/matplotlib-tuda',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.8'
+)
